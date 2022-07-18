200958bc947dbae718f5f939ae7c2a6d65ce163b
Bupka <ejup.pllashniku@gmail.com>
2022-07-18 16:24:04 +0200

    about section branch

--- src/components/styles/Container.styled.js
+++ src/components/styles/Container.styled.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   width: 960px;
-  height: 100vh;
+  min-height: 100vh;
   max-width: 100%;
   padding: 0;
   margin: 0 auto;
